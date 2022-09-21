4876b556a636e8a549bec7476b331296b3d8f82a
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-09-21 18:16:08 +0800

    recursion

--- JavaScript/dsa.js
+++ JavaScript/dsa.js
@@ -8,7 +8,7 @@ function linearSearch(array, key) {
   }
   return -1;
 }
-
+//test
 function verifyLinearSearch(index){
   if(index != -1){
     console.log("Target found at index: ",index)
@@ -38,3 +38,46 @@ function binarySearch(array, key){
   }
   return -1
 }
+//test
+function verifyBinarySearch(index){
+  if(index != -1){
+    console.log("Target found at index: ",index)
+  }else{
+    console.log("Target found in the list")
+  }
+}
+
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+const result = binarySearch(numbers, 14)
+
+verifyBinarySearch(result)
+
+//Recursive Binary Search
+function recursiveBinarySearch(array, key) {
+  if(array.length == 0){
+    return false
+  }else{
+    const middle = Math.floor(array.length/2)
+    if(array[middle] == key){
+      return true
+    }else{
+      if(array[middle] < key){
+          return recursiveBinarySearch(array.slice(middle + 1), key)
+      }else{
+        return recursiveBinarySearch(array.slice(0, middle), key)
+      }
+    }
+  }
+}
+//test
+function verifyRecursiveBinary(result){
+  console.log("Target found: ", result)
+}
+const numbers = [1,2,3,4,5,6,7,8]
+const result =  recursiveBinarySearch(numbers, 12)
+verifyRecursiveBinary(result)
+
+const result =  recursiveBinarySearch(numbers, 6)
+verifyRecursiveBinary(result) 
+
+//1:35
