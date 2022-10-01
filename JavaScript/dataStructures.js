12a6ca5b4a5c18a86fbf465358329b0210e90ef0
Charles-CarM <103493003+Charles-CarM@users.noreply.github.com>
2022-10-01 19:03:41 +0800

    array operations

--- JavaScript/dataStructures.js
+++ JavaScript/dataStructures.js
@@ -1,3 +1,25 @@
 
 const newList = [1,2,3]
 const result = newList[0]
+
+if (newList.includes(1)){
+  console.log(true)
+}
+
+for(const items in newList){
+  if(items ===1){
+    console.log(true)
+  }
+}
+
+//add items to an array
+
+//end
+const numbers = []
+console.log(numbers.length)
+numbers.push(2)
+
+//start
+const another = []
+numbers.unshift()
+
